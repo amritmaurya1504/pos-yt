a74df42aea6d596be215e24f01605b681b57ad38
Amrit Raj <amrit.raj1504@gmail.com>
2024-07-09 15:24:55 +0530

    constants added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
-
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="text-3xl font-bold text-red-800 underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">POS System</h1>
     </>
   );
 }
